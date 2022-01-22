74eef36ee52e86f5e730ee021b2fb66e12b27ef8
Ben Fradj Khalil <khalil.benfradj11@gmail.com>
2022-01-22 13:48:22 +0100

    Déployérsrrlms

--- art24/models.py
+++ art24/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Articles(models.Model):
+class Articles(models.Model):   
     contenu=models.TextField()
     slug=models.SlugField(max_length=150)
     date_publication=models.DateTimeField(auto_now_add=True)
@@ -11,7 +11,9 @@ class Articles(models.Model):
         return self.contenu
 
 class Articles_principale(models.Model):
+    titre=models.CharField(max_length=150)
     contenu=models.TextField() 
+    information=models.TextField()
     slug=models.SlugField(max_length=150)
     date_publication=models.DateTimeField(auto_now_add=True)
     images=models.ImageField(default='default.jpg')
